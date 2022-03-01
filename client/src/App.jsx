dc8a158f336204c5a0ea76c124e5a74cd351859e
sakshamjaiswal <saksham.jaiswal@six30labs.io>
2022-03-01 21:20:44 +0530

    nav

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,14 @@ import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
 const App = () => {
   return (
     <div className="min-h-screen">
-     <div className="gradient-bg-welcome">
-       <Navbar/>
-       <Welcome/>
-     </div>
-     <Transactions />
-     <Footer />
+    <div className="gradient-bg-welcome">
+      <Navbar />
+      <Welcome />
     </div>
+    <Services />
+    <Transactions />
+    <Footer />
+  </div>
   );
 };
 
